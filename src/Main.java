8bbbd227cbbb38fb77d964833804b8a3885f4cf6
Iago Belo de Oliveira Vieira <Iago Belo de Oliveira Vieira>
2016-11-09 19:39:53 -0300

    Removido variáveis de testes.

--- src/Main.java
+++ src/Main.java
@@ -2,11 +2,8 @@
  * Created by iagobelo on 08/11/2016.
  */
 public class Main {
-    private static final int i = 1;
-    private static final int[] totalResources = new int[]{6, 4, 9, 5};
     private static Process[] processes;
     private static Banker banker;
-    private static int[] freeResources = totalResources;
 
     public static void main(String[] args) {
         banker = new Banker();
@@ -16,9 +13,5 @@ public class Main {
             processes[i] = new Process(i, banker);
             processes[i].start();
         }
-
-        //freeResources[1] = totalResources[1] - 3;
-        //System.out.println(freeResources[1]);
-        //System.out.println(totalResources[1]);
     }
 }
